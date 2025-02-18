f19350e18e39d9a6201877d84f42de93d7aa8be7
Yorick Nap <yorick@napmail.nl>
2025-02-18 11:40:00 +0100

    Update overlay function and added drawGradient

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,54 @@
 #include <iostream>
+#include <cmath>
 #include <opencv2/opencv.hpp>
 
 #include "../include/RepeatingRow.h"
 #include "../include/Row.h"
 #include "../include/StepFigure.h"
 
-void overlay(cv::Mat& image, cv::Mat& mask, const cv::Vec4b color)
+void overlay(const cv::Scalar& color, cv::Mat& background, cv::Mat& alpha, const double opacity = 1.0)
 {
-    const uchar alpha = color[3];
-    const cv::Vec3b colorNoAlpha(color[0], color[1], color[2]);
+    alpha *= opacity;
 
+    cv::Mat foreground(background.size(), background.type(), color);
 
-    for (int i = 0; i < image.rows; i++)
-    {
-        for (int j = 0; j < image.cols; j++)
-        {
-            const uchar pointAlpha = alpha * mask.at<uchar>(i, j) / 255;
-            image.at<cv::Vec3b>(i, j) = (image.at<cv::Vec3b>(i, j) * (255 - pointAlpha) / 255 + colorNoAlpha * pointAlpha / 255);
+    foreground.convertTo(foreground, CV_32FC3);
+    background.convertTo(background, CV_32FC3);
+    alpha.convertTo(alpha, CV_32FC3, 1.0/255);
+
+    multiply(alpha, foreground, foreground);
+    multiply(cv::Scalar::all(1.0)-alpha, background, background);
+
+    add(foreground, background, background);
+}
+
+void drawGradient(const cv::Scalar& color1, const cv::Scalar& color2, cv::Mat& image, double angle)
+{
+    const int rows = image.rows;
+    const int cols = image.cols;
+    image.create(rows, cols, CV_8UC3);
+
+    const double radian = angle * CV_PI / 180.0;
+    const double cosA = std::cos(radian);
+    const double sinA = std::sin(radian);
+
+    const double maxDistance = std::sqrt(rows * rows + cols * cols);
+
+    for (int y = 0; y < rows; ++y) {
+        for (int x = 0; x < cols; ++x) {
+            const double distance = (x * cosA + y * sinA);
+            double alpha = distance / maxDistance;
+            alpha = std::clamp(alpha, 0.0, 1.0);
+            cv::Scalar color = (1.0 - alpha) * color1 + alpha * color2;
+            image.at<cv::Vec3b>(y, x) = cv::Vec3b(color[0], color[1], color[2]);
         }
     }
-};
+}
 
 int main()
 {
-    cv::Mat image(5320, 6960, CV_8UC4, cv::Scalar(10, 10, 10, 255));
+    cv::Mat image(5320, 6960, CV_8UC3, cv::Scalar(10, 10, 10, 255));
+    drawGradient(cv::Scalar(6, 19, 68), cv::Scalar(12, 42, 126), image, 45);
 
     StepFigure(RepeatingRow({
                    0
@@ -60,14 +85,14 @@ int main()
                }, 5)
         .draw(image, cv::Point(6300, 1000), 1000);
 
-    //cv::Mat figure = cv::Mat::zeros(image.rows, image.cols, CV_8UC1);
+    cv::Mat alpha = cv::Mat::zeros(image.rows, image.cols, CV_8UC3);
     StepFigure(RepeatingRow({
                    0
                }), {
                    2 * CV_PI * 101 / 240
                }, 240)
-        .draw(image, cv::Point(4000, 5500), 10000);
-    //overlay(image, figure, cv::Scalar(255, 255, 0, 200));
+        .draw(alpha, cv::Point(4000, 5500), 10000);
+    overlay(cv::Scalar(4, 137, 255), image, alpha, 0.5);
 
     StepFigure(RepeatingRow({
                    0
@@ -75,7 +100,6 @@ int main()
                    2 * CV_PI * 6 / 20
                }, 20)
         .draw(image, cv::Point(5000, 700), 1000);
-
 
     if (!imwrite("result.jpg", image))
     {
